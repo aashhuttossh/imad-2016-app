1ab5c559301fd803dae47352562fbc14461624a8
aashhuttossh <aashhuttossh@gmail.com>
2016-09-25 01:55:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/hello', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'hello.html'));
 });
 
