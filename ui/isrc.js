4cb69e8ad8b5261dcffc5130afe01d1dc2d18a2f
aashhuttossh <aashhuttossh@gmail.com>
2016-10-09 00:28:13 +0530

    [imad-console] Updates ui/isrc.js

--- ui/isrc.js
+++ ui/isrc.js
@@ -1,13 +1,3 @@
     
     
-   // alert("I am in java Scrip");
-    
  
-    
-    //var counter=document.getElementById("Count");
-    function msg1(){
-    cnt=cnt+1;
-    
-   document.getElementById("val").innerHTML = cnt ;
-    };
-
