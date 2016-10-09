8664a7594e117b39e8b921dac1854472d8c2e3c4
aashhuttossh <aashhuttossh@gmail.com>
2016-10-09 15:53:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,9 @@
 console.log('Loaded!');
+
 prompt('hello bro! ');
  
     var pos;
     function mov1(){
         var img=document.getElementById("img").innerHTML;
-        
+        img.style.width=img.style.width+10;
     }
