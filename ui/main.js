d442cd6572af7c7ca4fdecd0c80716cde09c6c66
aashhuttossh <aashhuttossh@gmail.com>
2016-10-10 12:17:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,6 @@
 console.log('Loaded!');
-
+var cnter=0;
+var button=document.getElementById("but1");
     /*var pos;
     function mov1(){
         var img=document.getElementById("img");
@@ -9,14 +10,20 @@ console.log('Loaded!');
 var request=new XMLHttpRequest();
 request.onreadystatechange = function (){
 
+    if(request.readyState===XMLHttpRequest.DONE)
+        if(request.status===200)
+          {  var cnter=request.responseText;
+               document.getElementById("i1").innerHTML=cnter;
+          }
+
+
+    
     
 }
 
 
-var button=document.getElementById("but1");
-var cnter=0;
 
-function pressed () {
-    cnter=cnter+1;
-    document.getElementById("i1").innerHTML=cnter;
+
+
+    
 }
