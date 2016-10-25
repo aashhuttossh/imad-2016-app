7b0d2537eab0103be0f038ad6e518125ed185831
aashhuttossh <aashhuttossh@gmail.com>
2016-10-25 23:48:40 +0530

    [imad-console] Updates ui/isrc.js

--- ui/isrc.js
+++ ui/isrc.js
@@ -4,13 +4,13 @@
 // alert("I am in java Scrip");
     
  var cnt=0;
-     var htm=`<marquee scrollamount=${cnt}> hello </marquee>`;
+     
     //var counter=document.getElementById("Count");
     function msg1(){
     cnt=cnt+1;
     
    document.getElementById("val").innerHTML = cnt ;
-   
+   var htm=`<marquee scrollamount=${cnt}> hello </marquee>`;
   
   document.getElementById("inp").innerHTML=htm;
     }
@@ -19,8 +19,8 @@
     function msg2(){
     cnt=cnt-1;
     
-   //document.getElementById("val").innerHTML = cnt ;
+   document.getElementById("val").innerHTML = cnt ;
     var htm=`<marquee scrollamount=${cnt}> hello </marquee>`;
-  
+ 
   document.getElementById("inp").innerHTML=htm;
     }
