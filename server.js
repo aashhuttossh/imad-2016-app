9c0cc77cd463433a1223f69d0a07d749c5e6c011
aashhuttossh <aashhuttossh@gmail.com>
2016-11-09 20:33:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,6 +68,7 @@ var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
   //make req
   //return response
+  var inp=document.getElementById("inp");
   pool.query('SELECT * FROM test',function(err,result){
       if(err){
           res.status(500).send(err.toString());
