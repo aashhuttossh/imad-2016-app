c41957581bbaa9129285574edbd0e49bd8d0e576
aashhuttossh <aashhuttossh@gmail.com>
2016-11-09 21:07:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,9 @@
 console.log('Loaded!');
 var cnter=0;
 var button=document.getElementById("but1");
-    var pos;
+var pos;
+var sql;
+
     
     function pressed(){
        counter=counter+1;
@@ -10,7 +12,7 @@ var button=document.getElementById("but1");
         img.style.width=750;*/
     }
     
-    var counter=0;
+var counter=0;
 var request=new XMLHttpRequest();
 /*button.onclick=function () {
 
@@ -19,6 +21,11 @@ var request=new XMLHttpRequest();
 request.open('GET','http://aashhuttossh.imad.hasura-app.io/counter',true);
 request.send(null);
 
+
+
+function myFunction() {
+     sql=document.getElementById("demo");
+}
 
 
 
