cff2f27996b923b337a6fdd9e59533264f85007d
aashhuttossh <aashhuttossh@gmail.com>
2016-11-11 19:35:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get('/test-db', function (req, res) {
 
 
 
-app.get('/articles/articleName',function(req,res){
+app.get('/articles/articeOne',function(req,res){
     var articleName=req.params.articleName;
     pool.query("SELECT * FROM article",function(err,result)
     {if(err)
@@ -97,7 +97,7 @@ app.get('/articles/articleName',function(req,res){
         if(result.rows.length===0){
             res.status(404).send('Article not found');
         }
-        else{ var articleDara=result.rows[0];
+        else{ var articleData=result.rows[0];
         res.send(createTemplate(articleData));
             
         }
